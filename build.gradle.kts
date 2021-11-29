0ad52c9df8646f78a3496a6916aeb5d254079b87
Gerard Paligot <gerard.paligot@gmail.com>
2021-11-29 22:53:07 +0100

    chore: Update Gradle and AGP plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.2.0-alpha02")
+        classpath("com.android.tools.build:gradle:7.2.0-alpha05")
         classpath("org.jetbrains.compose:compose-gradle-plugin:${Versions.jbCompose}")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
     }
